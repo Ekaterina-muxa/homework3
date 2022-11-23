cb683c6e82a4309f77e206974a44736670b23c12
«Ekaterina» <katuxa-muxa@mail.ru>
2022-11-23 10:38:13 +0200

    вторая задача

---
+++ hw2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число numberA: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число numberB: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число numberC: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > numberB) max = numberA; 
+if (numberB > numberA) max = numberB; 
+if (numberC > numberA) max = numberC; 
+if (numberC > numberB) max = numberC; 
+Console.Write("max = ");
+Console.WriteLine(max);
