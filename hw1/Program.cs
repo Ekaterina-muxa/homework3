5742eb4c3e751cc45a21bf1b9ff61390b01c89a7
«Ekaterina» <katuxa-muxa@mail.ru>
2022-11-23 10:31:35 +0200

    первая задача

---
+++ hw1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+int min = numberB;
+
+if (numberA > numberB) max = numberA; min = numberB;
+if (numberB > numberA) max = numberB; min = numberA;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+
+
+
+
