1c7b4d67f4d17c8b0832b5b01a91c5542d7b5481
«Ekaterina» <katuxa-muxa@mail.ru>
2022-11-23 10:40:41 +0200

    третья задача

---
+++ hw3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число numberA: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int result = numberA % 2; 
+
+if(result == 0)
+{
+    Console.WriteLine ("четное число");
+}
+else
+{
+    Console.Write("нечетное число");
+}
