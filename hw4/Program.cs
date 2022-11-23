a7033ed8aa19c3885564ef4f1bf2fa1a54ea6bb0
«Ekaterina» <katuxa-muxa@mail.ru>
2022-11-23 10:43:04 +0200

    Четвертая задача на ряд четных чисел

---
+++ hw4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+
+while (count <= N)
+{
+   int result = count % 2; 
+   if(result == 0)
+        {
+         Console.WriteLine (count);
+        }
+    count ++;
+}
